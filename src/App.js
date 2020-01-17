4ff9ffd72bd358bb0ab8febf795f0079382608b3
alakdam07 <47625626+alakdam07@users.noreply.github.com>
2020-01-17 21:49:34 +0200

    Online Movie App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Drama from "./components/Drama";
+import Popular from "./components/Popular";
+import TopRated from "./components/TopRated";
+import Upcoming from "./components/Upcoming";
+import TopTv from "./components/TopTv";
+import Search from "./components/Search";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <br></br>
+      <Search />
+      <h5>Popular Movies</h5>
+      <Popular />
+      <h5>Top Rated Movies</h5>
+      <TopRated />
+      <h5>Trending</h5>
+      <Drama />
+      <h5>Upcoming Movies</h5>
+      <Upcoming />
+      <h5>Top Rated Tv Program</h5>
+      <TopTv />
     </div>
   );
 }
